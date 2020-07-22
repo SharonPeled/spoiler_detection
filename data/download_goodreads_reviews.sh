d921252a518e0864ebfe13c1c995132ce2892dc1
SharonPeled <ssharon95@gmail.com>
2020-07-22 13:22:37 +0300

    cleaning and ordering

---
+++ data/download_goodreads_reviews.sh
@@ -0,0 +1,6 @@
+function gdrive_download () {
+CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+rm -rf /tmp/cookies.txt
+}
+gdrive_download 196W2kDoZXRPjzbTjM6uvTidn6aTpsFnS goodreads_reviews_spoiler.json.gz
