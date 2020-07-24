b90490c3d2e15b3df734c3c526aca1c83b424d15
SharonPeled <ssharon95@gmail.com>
2020-07-24 13:34:38 +0300

    working version

--- code/utils.py
+++ code/utils.py
@@ -8,11 +8,12 @@ DATASET_PATH = '../data/goodreads_reviews_spoiler.json'
 DATA_DIR = '../data/'
 TEST_RATIO = 0.2
 VALID_RATIO = 0.2
+PROCESSED_SUFFIX = "_processed"
 
 
 def clean_word(word):
     # TODO: may need to change this for the pretrained embedding
-    return word.translate(str.maketrans('', '', string.punctuation)) # removing punctuation
+    return word.translate(str.maketrans('', '', string.punctuation)).lower() # removing punctuation
 
 
 def save_pickle(obj, filename):
@@ -23,3 +24,15 @@ def save_pickle(obj, filename):
 def load_pickle(filename):
     with open(os.path.join(DATA_DIR, filename), 'rb') as file:
         return pickle.load(file)
+    
+    
+def safe_load_pickle(filename):
+    if not os.path.isfile(os.path.join(DATA_DIR, filename)):
+        return False, None
+    print(f"Loading existing {filename} .")
+    return True, load_pickle(filename)
+
+
+def save_model(model, version):
+    save_pickle(model,f"model_{version}")
+    
