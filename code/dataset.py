102ae99803fbe41cd1f22ce9d13f0e43f7bc04af
SharonPeled <ssharon95@gmail.com>
2020-08-03 21:36:12 +0300

    adding word features

--- code/dataset.py
+++ code/dataset.py
@@ -7,19 +7,23 @@ import pickle
 import string
 from tqdm import tqdm
 from utils import *
-
+import numpy as np
 
 # instance attributes = (review_input,review_labels,book_id,user_id,timestamp,rating)
 class SpoilerDataset(Dataset):
     def __init__(self, filename,
                  word_to_id_filename="word_to_id.pickle",id_to_word_filename="id_to_word.pickle",
-                 book_to_id_filename="book_to_id.pickle", user_to_id_filename="user_to_id.pickle", load=True):
+                 book_to_id_filename="book_to_id.pickle", user_to_id_filename="user_to_id.pickle",
+                 book_reviews_count_filename="book_reviews_count.pickle",
+                 word_per_book_count_filename="word_per_book_count.pickle", load=True):
         super().__init__()
         self.filename = filename
         self.word_to_id = load_pickle(word_to_id_filename)
         self.id_to_word = load_pickle(id_to_word_filename)
         self.book_to_id = load_pickle(book_to_id_filename)
         self.user_to_id = load_pickle(user_to_id_filename)
+        self.book_reviews_count = load_pickle(book_reviews_count_filename)
+        self.word_per_book_count = load_pickle(word_per_book_count_filename)
         self.data = None
         if load:
             result, obj = safe_load_pickle(self.generate_processed_name())
@@ -28,12 +32,6 @@ class SpoilerDataset(Dataset):
                 return
         print("Generating ...")
         self.create_dataset()
-                
-        
-    
-    @classmethod
-    def load_dataset(load_dataset):
-        self.data = load_pickle(load_dataset)
     
     def generate_processed_name(self):
         return self.filename.split(".")[0] + PROCESSED_SUFFIX + "." + self.filename.split(".")[1]
@@ -76,4 +74,25 @@ class SpoilerDataset(Dataset):
     def load_pretrained_embeddings(self, dim):
         vectors = GloVe(name="6B",dim=dim)
         return torch.stack([vectors[self.id_to_word[ind]] for ind in tqdm(range(len(self.id_to_word)))])
-        
+    
+    def get_tf_idf_features_tensor(self, sentence_batch, book_id):
+        return torch.tensor(
+            [
+                [self.generate_word_features(word_id, book_id) for word_id in sentence]
+                for sentence in sentence_batch
+            ]
+        )
+    
+    def generate_word_features(self, word_id, book_id):
+        word_id = word_id.item()
+        book_id = book_id.item()
+        word = self.id_to_word[word_id]
+        if word == SPECIAL_CHARACTER:
+            return [0.0, 0.0, 0.0]
+        num_reviews = self.book_reviews_count[book_id]
+        num_tot_books = len(self.book_reviews_count)
+        word_occ_in_reviews = self.word_per_book_count[word][book_id]
+        tot_book_word_occ = len(self.word_per_book_count[word])
+        DF = word_occ_in_reviews/num_reviews
+        IIF = np.log((IDF_SMOOTHING+tot_book_word_occ)/(IDF_SMOOTHING+num_tot_books))
+        return [DF, IIF, DF*IIF]
